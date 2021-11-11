d93b1d9037053521687d4e86aaa294664a82407b
Sitaram Adarkar <37792832+gnaesh@users.noreply.github.com>
2021-11-11 12:17:10 +0530

    s
    s

---
+++ src/sitaram_Adarkar_Lab2/q4/lab2q4.java
@@ -0,0 +1,34 @@
+package sitaram_Adarkar_Lab2.q4;
+
+import java.util.Arrays;
+
+public class lab2q4
+{
+	public int modifyArray(int[] a)
+	{	int j = 0;
+		for (int i = 0; i < a.length-1; i++)
+		{
+			if (a[i] != a[i+1])
+			{	a[j] = a[i];
+				j++;
+			}
+		}
+        a[j++]=a[a.length-1];
+		Arrays.sort(a);
+		return j;
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] a = { 1, 4, 5, 1,3 };
+		lab2q4 ref = new lab2q4();
+		Arrays.sort(a);
+		int len = ref.modifyArray(a);
+		for (int i = len-1; i >= 0; i--) 
+		{
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
